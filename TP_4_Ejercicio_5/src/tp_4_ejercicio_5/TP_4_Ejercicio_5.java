c1eb9802629d6ebc5a180aa95721ab57ffd8beb1
dylancondori <dcondoricorazon@gmail.com>
2026-05-08 14:08:49 -0300

    hecho el 5

--- TP_4_Ejercicio_5/src/tp_4_ejercicio_5/TP_4_Ejercicio_5.java
+++ TP_4_Ejercicio_5/src/tp_4_ejercicio_5/TP_4_Ejercicio_5.java
@@ -3,11 +3,11 @@ public class TP_4_Ejercicio_5 {
     public static void main(String[] args) {
         int [] vector = new int [20];
         int i = 0;
-        while (i < 20){
+        do{
             vector[i] = ( i+1 )*2;
             i++;
-        }
-        System.out.println("Primeros 20 numeros pares:");
+        } while (i < 20);
+        System.out.println("Los primeros 20 numeros pares");
         for(int a = 0;a < 20;a++){
             System.out.println(vector[a]);
         }
